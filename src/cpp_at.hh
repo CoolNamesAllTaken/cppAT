616f5a784e685ff7314aae6737a9ff17f5962434
John McNelly <jkailimcnelly@gmail.com>
2024-05-09 21:58:15 -0700

    Add error and success macros

--- src/cpp_at.hh
+++ src/cpp_at.hh
@@ -172,4 +172,12 @@ private:
         return false;                                                                                 \
     }
 
+#define CPP_AT_SUCCESS              \
+    CppAT::cpp_at_printf("OK\r\n"); \
+    return true;
+
+#define CPP_AT_ERROR                    \
+    CppAT::cpp_at_printf("ERROR\r\n");  \
+    return false;
+
 #endif /* _CPP_AT_HH_ */
